5b7ce46c6998182a1b5da21f646e45d722028d3b
AGRedQ <dinhminhquan345@gmail.com>
2025-06-13 18:12:45 +0700

    Simple Intent with API, pipeline functions made

--- test.py
+++
@@ -1,7 +0,0 @@
-import spacy
-import spacy_transformers
-
-print("spaCy loaded.")
-print("Transformers module:", spacy_transformers)
-nlp = spacy.load("en_core_web_trf")
-print("Transformer model loaded successfully.")
